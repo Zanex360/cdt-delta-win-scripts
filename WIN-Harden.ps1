29646f80632468216654ae0d541da9decccbc339
Alexander Vyzhnyuk <52129941+Zanex360@users.noreply.github.com>
2026-02-17 19:08:34 -0500

    Included extra options for cell-win-03 5 minute

--- WIN-Harden.ps1
+++ WIN-Harden.ps1
@@ -5,16 +5,19 @@ Write-Host "Start Time:" (Get-Date)
 Write-Host "`n[1] Local Administrators:"
 net localgroup Administrators
 
+Write-Host "`n[2] Logged-in Users:"
+query user
+
 # Scheduled Tasks
-Write-Host "`n[2] Scheduled Tasks:"
+Write-Host "`n[3] Scheduled Tasks:"
 schtasks /query /fo LIST /v
 
 # Running Processes
-Write-Host "`n[3] Running Processes:"
+Write-Host "`n[4] Running Processes:"
 tasklist
 
 # Disable Remote Registry
-Write-Host "`n[4] Disabling Remote Registry"
+Write-Host "`n[5] Disabling Remote Registry"
 try {
     sc.exe stop RemoteRegistry
     sc.exe config RemoteRegistry start= disabled
@@ -22,11 +25,11 @@ try {
 catch { Write-Host "Remote Registry already disabled" }
 
 # Enable Firewall
-Write-Host "`n[5] Enabling Firewall"
+Write-Host "`n[6] Enabling Firewall"
 netsh advfirewall set allprofiles state on
 
 # Recent Logins
-Write-Host "`n[6] Recent Logins:"
+Write-Host "`n[7] Recent Logins:"
 Get-EventLog Security -InstanceId 4624 -Newest 10
 
 Write-Host "`n=== Completed ==="
